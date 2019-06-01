cade377d3a26dbbf86f7bac8dc8656a918d8d8f5
TXWHITE <trevorwhitejr@gmail.com>
2019-06-01 19:00:16 -0400

    Created function to strip code from string

--- autoAnswerBurrito.py
+++ autoAnswerBurrito.py
@@ -1,1 +1,10 @@
-#test trevor
+def findCode(text):
+    test = text.split()
+    counter = 0
+    code = 'Could not find code'
+    while counter < len(test) :
+        tempWord = test[counter]
+        if tempWord[0:4] == 'FREE':
+            code = tempWord
+        counter = counter + 1
+    return code
