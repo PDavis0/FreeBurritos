d20df58818444dbc1ea40438e936034e357698f9
Parker <pkdavisf@gmail.com>
2019-06-01 19:16:57 -0400

    working code finder

--- autoAnswerBurrito.py
+++ autoAnswerBurrito.py
@@ -3,21 +3,8 @@ import twilio # phone number
 
 # Chipotle tweets -> grab "@chipotleTweets"s' tweet -> find code -> twilio text 888222
 
-#find code
 
-#for each line, find words, if word.length==8 && substring(0,3) =="FREE" , set = burritoCode
-
-#import file and get code
-String code = " "
-tweet = open("chipotleTweet",r)
-tweetString = tweet.read()
-code = findCode(tweetString)
-
-
-
-
-
-def findCode(text):
+def findCode(text) :
     test = text.split()
     counter = 0
     code = 'Could not find code'
@@ -27,3 +14,12 @@ def findCode(text):
             code = tempWord
         counter = counter + 1
     return code
+
+#find code
+code = ' '
+tweet = open('sampleChipotleTweet.txt','r')
+tweetString = tweet.read()
+code = findCode(tweetString)
+print(code)
+
+
